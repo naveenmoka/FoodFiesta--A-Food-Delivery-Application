9e58cde9497c03e4b2014b121a53877345678963
naveenmoka <naveenmoka2004@gmail.com>
2025-12-02 11:27:12 +0530

    Fix the Deployment error - contact Import issue

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
-import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 import userContext from "./utils/userContext";
 import Cart from "./components/Cart";
 import Footer from "./components/Footer";
+
 const Contact = lazy(() => import("./components/Contact"));
 
 const AppLayout = () => {
